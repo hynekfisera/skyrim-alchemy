4e39471187e4622593253db7d3f16e424c13b371
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2022-04-20 15:07:03 +0200

    Multiple

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import data from "../resources/alchemy.json";
-import { useState, useEffect, SyntheticEvent } from "react";
+import { useState, SyntheticEvent } from "react";
 import Ingredient from "../components/Ingredient";
 
 const Home: NextPage = () => {
-  const [filter, setFilter] = useState("all");
+  const [filteredEffects, setFilteredEffects] = useState<String[]>([]);
 
   const { effects, ingredients } = data;
 
@@ -18,19 +17,36 @@ const Home: NextPage = () => {
       </Head>
       <main className="p-6">
         <h1 className="text-xl font-medium">Skyrim - Alchemy minigame</h1>
-        <section className="my-4">
-          <select value={filter} onChange={(e: SyntheticEvent<HTMLSelectElement>) => setFilter((e.target as HTMLSelectElement).value)}>
-            <option value="all">All Effects</option>
-            {effects.map((effect) => (
-              <option key={effect} value={effect}>
-                {effect}
-              </option>
-            ))}
-          </select>
+        <section className="my-4 flex-col">
+          {effects.map((effect) =>
+            filteredEffects.includes(effect) ? (
+              <div
+                key={effect}
+                className="text-green-500"
+                onClick={() => {
+                  setFilteredEffects((f) => f.filter((e) => e !== effect));
+                }}
+              >
+                - {effect}
+              </div>
+            ) : (
+              <div
+                key={effect}
+                className="text-red-500"
+                onClick={() => {
+                  const temp = filteredEffects;
+                  temp.push(effect);
+                  setFilteredEffects(temp);
+                }}
+              >
+                + {effect}
+              </div>
+            )
+          )}
         </section>
         <section className="grid grid-cols-6 gap-16">
-          {filter === "all" && ingredients.map((ingredient, i) => <Ingredient ingredient={ingredient} key={i} className="w-full" />)}
-          {filter !== "all" && ingredients.filter((ingredient) => ingredient.effects.includes(filter)).map((ingredient, i) => <Ingredient ingredient={ingredient} key={i} />)}
+          {filteredEffects.length === 0 && ingredients.map((ingredient, i) => <Ingredient ingredient={ingredient} key={i} className="w-full" />)}
+          {filteredEffects.length > 0 && ingredients.filter((ingredient) => ingredient.effects.every((e) => filteredEffects.includes(e))).map((ingredient, i) => <Ingredient ingredient={ingredient} key={i} />)}
         </section>
       </main>
     </>
